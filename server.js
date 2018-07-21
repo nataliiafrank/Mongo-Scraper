0a7ba36113bb57f1c111fcaf9d611a1520cde24b
Nataliia Frank <nataliiafrank@gmail.com>
2018-07-21 15:42:26 -0400

    Updated mongoose config

--- server.js
+++ server.js
@@ -26,7 +26,16 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/8080");
+// mongoose.connect("mongodb://localhost/8080");
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/8080";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 //Routs
 app.get("/", function (req, res) {
